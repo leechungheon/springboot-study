aaead7b8ead754fd65ff4f327de02b673f9290b9
leechung6311@gmail.com <leechung6311@gmail.com>
2026-04-30 19:42:06 +0900

    4/30

--- src/main/java/mybatis/dao/MyTableMapper.java
+++ src/main/java/mybatis/dao/MyTableMapper.java
@@ -1,14 +1,17 @@
 package mybatis.dao;
 
 import com.example.springedu.domain.MyTableDTO;
-import org.apache.ibatis.annotations.*;
-
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface MyTableMapper {
-	@Select("select my_name, my_age from my_table")
-	public List<MyTableDTO> list();
-	@Insert("insert into my_table values (#{myName}, #{myAge})")
-	public boolean insert(MyTableDTO visitor);
+
+  @Select("select my_name, my_age from my_table")
+  public List<MyTableDTO> list();
+
+  @Insert("insert into my_table values (#{myName}, #{myAge})")
+  public boolean insert(MyTableDTO visitor); // df
 }
--- src/test/java/com/example/springedu/MyTableMapperInsertTest.java
+++ src/test/java/com/example/springedu/MyTableMapperInsertTest.java
@@ -1,4 +1,4 @@
-package com.example.springedu;
+package com.example.springedu;//package com.example.springlab;
 
 import com.example.springedu.domain.MyTableDTO;
 import mybatis.dao.MyTableMapper;
@@ -8,25 +8,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Scanner;
-
 @SpringBootTest
 class MyTableMapperInsertTest {
-	@Autowired
-	MyTableMapper dao;
-	@Test
-	@Transactional 	// 메서드를 하나의 트랜잭션(작업 단위)으로 묶어주는 Spring의 선언형 트랜잭션 처리 어노테이션
-							// 메서드 안에서 실행되는 여러 DB 작업을 “전부 성공(commit)” 또는 “전부 실패(rollback)”로 처리
-	@Rollback(false)
-	void insert() {
-		MyTableDTO dto;
-		for(int i=1; i < 6; i++) {
-			dto = new MyTableDTO();
-			dto.setMyName("둘리"+i);
-			dto.setMyAge(10+i);
-			boolean result = dao.insert(dto);
-			if(result)
-				System.out.println("둘리"+i+" 입력 완료");
-		}
-	}
+
+  @Autowired
+  MyTableMapper dao;
+
+  @Test
+  @Transactional  // 메서드를 하나의 트랜잭션(작업 단위)으로 묶어주는 Spring의 선언형 트랜잭션 처리 어노테이션
+  // 메서드 안에서 실행되는 여러 DB 작업을 “전부 성공(commit)” 또는 “전부 실패(rollback)”로 처리
+  @Rollback(false)
+  void insert() {
+    MyTableDTO dto;
+    for (int i = 1; i < 6; i++) {
+      dto = new MyTableDTO();
+      dto.setMyName("둘리" + i);
+      dto.setMyAge(10 + i);
+      boolean result = dao.insert(dto);
+      if (result) {
+        System.out.println("둘리" + i + " 입력 완료");
+      }
+    }
+  }
 }
